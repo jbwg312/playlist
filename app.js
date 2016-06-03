d309ba94adec65a3951c1f0fa9a84c404cf21b7f
Jerry Benton Walden Goebel <jbwg312@Jerrys-MacBook-Pro.local>
2016-06-03 11:52:15 -0600

    play list splash page done

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ document.addEventListener("DOMContentLoaded", function(event){
   addPix();
 })
 
-
-
-
 function addPix(){
   var alb1 = document.getElementById("alb1");
   var alb2 = document.getElementById("alb2");
@@ -20,13 +17,15 @@ function addPix(){
         var arr = results.results;
         var img1 = arr[Math.floor(Math.random()*arr.length)].cover_art;
         var img2 = arr[Math.floor(Math.random()*arr.length)].cover_art;
+        var img3 = arr[Math.floor(Math.random()*arr.length)].cover_art;
+        for(var i = 0; i < 28; i++){
           if(img1 === img2){
             img2 = arr[Math.floor(Math.random()*arr.length)].cover_art;
           }
-        var img3 = arr[Math.floor(Math.random()*arr.length)].cover_art;
           if(img3 === img1 ||  img3 === img2){
             img3 = arr[Math.floor(Math.random()*arr.length)].cover_art;
           }
+        }
           alb1.innerHTML = "<img src=\'images/" + img1 + "\'/>";
           alb2.innerHTML = "<img src=\'images/" + img2 + "\'/>";
           alb3.innerHTML = "<img src=\'images/" + img3 + "\'/>";
